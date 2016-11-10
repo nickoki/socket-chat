9f009fb1dfa16d490da3ce44be9318be3da90478
Nick Oki <nick.oki@live.com>
2016-11-10 12:14:06 -0500

    Data persistence

--- index.js
+++ index.js
@@ -1,15 +1,37 @@
 var app = require('express')()
 var http = require('http').Server(app)
 var io = require('socket.io')(http)
+var mongoose = require('mongoose')
+
+mongoose.connect("mongodb://localhost/socket-chat")
+mongoose.Promise = global.Promise
+
+var Message = mongoose.model("Message", new mongoose.Schema({
+  body: String,
+  timestamp: Date
+}))
+
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html')
+})
+
+app.get('/api/messages', function(req, res) {
+  Message.find({}).lean().exec().then(messages => {
+    res.json(messages)
+  })
 })
 
 io.on('connection', socket => {
   socket.on('chat message', msg => {
     console.log('message: ' + msg)
     io.emit('chat message', msg)
+    if (msg) {
+      Message.create({
+        body: msg,
+        timestamp: Date.now()
+      })
+    }
   })
 })
 
